8adf47dc170970682c2668a364bf819938241d84
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-09-21 23:49:43 -0400

    Add Segues to transition to ViewControllers

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -2,9 +2,8 @@
 //  AppDelegate.swift
 //  Flash Chat iOS13
 //
-//  Created by Angela Yu on 21/10/2019.
-//  Copyright © 2019 Angela Yu. All rights reserved.
-//
+//  Created by Mohammed Hossain on 21/09/2020.
+
 
 import UIKit
 
