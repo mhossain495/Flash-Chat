48a0b799149813950ae5589f63a3c3f2a0980eb8
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-10-01 23:36:58 -0400

    Add Firestore database to store message data

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
+        // Initialize an instance of Cloud Firestore
+        let db = Firestore.firestore()
+        
         return true
     }
 
