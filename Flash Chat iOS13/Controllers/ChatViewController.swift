1206d918cdae9719013668b467a2b957ef678b63
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-10-02 22:11:16 -0400

    Retrieve data from Firestore

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -16,9 +16,7 @@ class ChatViewController: UIViewController {
     // Initialize an instance of Cloud Firestore
     let db = Firestore.firestore()
     
-    var messages: [Message] = [
-        Message(sender: "test@yahoo.com", body: "Hey!"),
-        Message(sender: "test2@yahoo.com", body: "Hello!")]
+    var messages: [Message] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,22 +30,35 @@ class ChatViewController: UIViewController {
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
 
+        loadMessages()
     }
-    
-    @IBAction func sendPressed(_ sender: UIButton) {
-        // Assign text in message text field to messageBody
-        // Assign currently signed in user email to messageSender
-        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
-            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField:messageBody]) { (error) in
+        
+        func loadMessages() {
+            messages = []
+            
+            db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
                 if let e = error {
-                    print("There was an issue saving data to firestore, \(e)")
+                    print("There was an issue retrieving data from Firestore. \(e)")
                 } else {
-                    print("Successfully saved data")
+                    if let snapshotDocuments = querySnapshot?.documents {
+                        for doc in snapshotDocuments {
+                            let data = doc.data()
+                            if let messagesender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messagesender, body: messageBody)
+                                self.messages.append(newMessage)
+                                
+                                // Load message data
+                                DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
     
+        
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         
         let firebaseAuth = Auth.auth()
@@ -72,7 +83,7 @@ extension ChatViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        as! MessageCell
+            as! MessageCell
         cell.label.text = messages[indexPath.row].body
         return cell
     }
