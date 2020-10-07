7197ecbcbfb74d2da1216342291aeebf66bf2bed
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2020-10-06 22:29:07 -0400

    Hide navigation bar in welcome screen and display in log in screen

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,10 +12,22 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: CLTypingLabel!
     
+    // Hide navigation bar in welcome screen before current view is displayed
+    // Use super when overriding superclass function
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(<#T##animated: Bool##Bool#>)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    // Load navigation bar; Called before view is removed from hierarchy
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(<#T##animated: Bool##Bool#>)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         titleLabel.text = K.appName
-        
     }
 }
